86d2721b3336e5a5782d3dd864ce8d911d880a2c
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-02-11 22:20:05 -0500

    Refactor function

--- logistics_simulator/src/components/weightVolumen/weightVolumen.js
+++ logistics_simulator/src/components/weightVolumen/weightVolumen.js
@@ -1,16 +1,13 @@
+function weightVolumen(boxWidth, boxLength, boxHeight) {
+  //Factor de equialencia para convertir cm3 a kg.
+  const volumetricWeightFactor = 400;
 
+  const volumenCm3 = boxHeight * boxLength * boxWidth;
+  const volumenMt = volumenCm3 / 1000000;
+  const weightVolumen = volumenMt * volumetricWeightFactor;
 
-function weightVolumen(boxWidth,boxLength,boxHeight) {
-   //Factor de equialencia para convertir cm3 a kg.
-   let volumetricWeightFactor = 400;
-
-   const volumenCm3 = boxHeight * boxLength * boxWidth;
-   const volumenMt = volumenCm3/1000000;
-   const weightVolumen = volumenMt * volumetricWeightFactor;
-
-   console.log("Peso volumen: " + weightVolumen)
-   return weightVolumen;
-
+  console.log("Peso volumen: " + weightVolumen);
+  return weightVolumen;
 }
 
-const  calculatedWeightVolumen = weightVolumen(57,37,24);
+const calculatedWeightVolumen = weightVolumen(57, 37, 24);
